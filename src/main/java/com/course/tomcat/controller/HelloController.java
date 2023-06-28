7e9fd4eb1e097d9d350c6c3cbb71d2d3485ad810
manjulbhatt <bhatt.manjul@gmail.com>
2023-06-29 00:09:26 +0530

    Update HelloController.java
    Updated for random method

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.course.tomcat.controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +12,11 @@ public class HelloController {
     @GetMapping(value = {"", "/", "/now"})
     String hello() {
         return "Now is " + LocalDateTime.now();
+    }
+
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
     }
 
 }
